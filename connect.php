015f2eafe711cb05337b9132528f68a9fef0595d
moThitima1612 <110826833+moThitima1612@users.noreply.github.com>
2022-12-13 16:45:36 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+
+
+// Create connection
+$conn = mysqli_connect("localhost", "root", "", "test1");
+
+ /*  echo "เชื่อมฐานข้อมูล" */
+?>
---
+++ index.php
@@ -0,0 +1,445 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>เว็บไซต์บริษัท</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  
+<link rel="icon" type="img/png" sizes="16x16" href="http://localhost/company/img/2250.png"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='UTF-8'>
+
+
+    <style>
+		.content {display:none;}
+		.preload { width:100px;
+			height: 200px;
+			position: fixed;
+			top: 30%;
+			left: 45%;
+		}
+		</style>
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+		<script>
+		$(function() {
+			$(".preload").fadeOut(2000,
+		function() {
+			$(".content").fadeIn(1000);
+		});
+		});
+		</script>
+	</head>
+	<body>
+    
+ 
+	
+		
+<style>
+
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+
+</style>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>เว็บไซต์บริษัท</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='UTF-8'>
+
+		<!-- <title>Simple Loader</title>
+
+		<style>
+		.content {display:none;}
+		.preload { width:100px;
+			height: 200px;
+			position: fixed;
+			top: 30%;
+			left: 45%;
+		}
+		</style>
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+		<script>
+		$(function() {
+			$(".preload").fadeOut(2000,
+		function() {
+			$(".content").fadeIn(1000);
+		});
+		});
+		</script>
+
+	</head>
+	<body>
+		<div class="preload"><img src="images/loader-128x/Preloader_3.gif"></div>
+		<div class="content"> -->
+
+  <style>
+    
+    body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+  .container {
+    padding: 80px 120px;
+  }
+  .person {
+    border: 10px solid transparent;
+    margin-bottom: 25px;
+    width: 80%;
+    height: 80%;
+    opacity: 0.7;
+  }
+  .person:hover {
+    border-color: #f1f1f1;
+  }
+  body {
+
+font-size: 18px;
+
+
+
+}
+  </style>
+  <!-- CSS
+    ================================================== -->
+   
+
+</head>
+
+<body>
+  
+
+          
+
+ 
+
+ <!-- Navbar -->
+ 
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#1" class="w3-bar-item w3-button w3-padding-large">หน้าหลัก</a>
+    <a href="#2" class="w3-bar-item w3-button w3-padding-large w3-hide-small">เกี่ยวกับ</a>
+    <a href="#3" class="w3-bar-item w3-button w3-padding-large w3-hide-small">บริการ</a>
+    <a href="#4" class="w3-bar-item w3-button w3-padding-large w3-hide-small">ผลงาน</a>
+    <a href="#5" class="w3-bar-item w3-button w3-padding-large w3-hide-small">ข่าวสาร</a>
+    <a href="#6" class="w3-bar-item w3-button w3-padding-large w3-hide-small">ติดต่อ</a>
+    <a href="#7" class="w3-bar-item w3-button w3-padding-large w3-hide-small">สมัครงาน</a>
+    <div class="w3-dropdown-hover w3-hide-small">
+      <button class="w3-padding-large w3-button" title="More">MORE <i class="fa fa-caret-down"></i></button>     
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="#" class="w3-bar-item w3-button">Merchandise</a>
+        <a href="#" class="w3-bar-item w3-button">Extras</a>
+        <a href="#" class="w3-bar-item w3-button">Media</a>
+      </div>
+     
+    </div>
+   
+    <P Align=right><img src="img/Tesla.png" height="60" width="160"></P> 
+
+    
+  </div>
+</div>
+
+<!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
+<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+  <a href="#1" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">หน้าหลัก</a>
+  <a href="#2" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">เกี่ยวกับ</a>
+  <a href="#3" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">บริการ</a>
+  <a href="#4" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">ผลงาน</a>
+  <a href="#5" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">ข่าวสาร</a>
+  <a href="#6" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">ติดต่อ</a>
+  <a href="#7" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">สมัครงาน</a>
+  <a href="#8" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">MERCH</a>
+ 
+</div>
+</div>
+
+          </ul>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    
+                    </div>
+                </div> 
+    
+            </div>
+<!-- <!DOCTYPE html>
+        <html>
+        <head>
+        <title>
+        CSChatThaiUI
+        - Web font preview
+        </title>
+        <style type='text/css'>
+
+        @font-face {
+         font-family: 'Lulu';
+         src: url('Lulu.eot?#iefix') format('embedded-opentype'),  url('Lulu.woff') format('woff'), url('Lulu.ttf')  format('truetype'), url('Lulu.svg#CSChatThaiUI') format('svg');
+         font-weight: normal;
+         font-size:50%;
+         font-style: normal;
+        }
+
+        body { font-family: 'Lulu' !important; }
+
+        </style>
+        </head>
+        <body>
+ 
+</body>  -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+ 
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="img/1.jpg" alt="New York" width="1500" height="500">
+        <div class="carousel-caption">
+          <h3>New York</h3>
+          <p>The atmosphere in New York is lorem ipsum.</p>
+        </div>      
+      </div>
+
+      <div class="item">
+        <img src="img/2.jpg" alt="Chicago" width="1500"height="500">
+        <div class="carousel-caption">
+        
+          <h3>Chicago</h3>
+          <p>Thank you, Chicago - A night we won't forget.</p>
+        </div>      
+      </div>
+    
+      <div class="item">
+        <img src="img/3.jpg" alt="Los Angeles" width="1500" height="500">
+        <div class="carousel-caption">
+          <h3>LA</h3>
+          <p>Even though the traffic was a mess, we had the best time playing at Venice Beach!</p>
+        </div>      
+      </div>
+    </div>
+    
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<div class="container text-center">
+  <h1>About Your Business</h1>
+  <p><em>We love music!</em></p>
+  <p>We have created a fictional band website. Lorem ipsum..</p>
+  <br>
+  
+  <div class="mySlides w3-display-container w3-center">
+    <img src="/w3images/chicago.jpg" style="width:100%">
+    <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
+      <h3>Chicago</h3>
+      <p><b>Thank you, Chicago - A night we won't forget.</b></p>    
+    </div>
+  </div>
+
+  <!-- The Band Section -->
+  <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+    <h2 class="w3-wide">เกี่ยวกับ</h2>
+    <p class="w3-opacity"><i>We love music</i></p>
+    <p class="w3-justify">We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-third">
+        <p>Name</p>
+        <img src="/w3images/bandmember.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:60%">
+      </div>
+      <div class="w3-third">
+        <p>Name</p>
+        <img src="/w3images/bandmember.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:60%">
+      </div>
+      <div class="w3-third">
+        <p>Name</p>
+        <img src="/w3images/bandmember.jpg" class="w3-round" alt="Random Name" style="width:60%">
+      </div>
+    </div>
+  </div>
+    
+<!-- The Tour Section -->
+<div class="w3-black" id="tour">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+      <h2 class="w3-wide w3-center">บริการ</h2>
+      <p class="w3-opacity w3-center"><i>Remember to book your tickets!</i></p><br>
+
+<ul class="w3-ul w3-border w3-white w3-text-grey">
+  <li class="w3-padding">September <span class="w3-tag w3-red w3-margin-left">Sold out</span></li>
+  <li class="w3-padding">October <span class="w3-tag w3-red w3-margin-left">Sold out</span></li>
+  <li class="w3-padding">November <span class="w3-badge w3-right w3-margin-right">3</span></li>
+</ul>
+
+<div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
+  <div class="w3-third w3-margin-bottom">
+    <img src="/w3images/newyork.jpg" alt="New York" style="width:100%" class="w3-hover-opacity">
+    <div class="w3-container w3-white">
+      <p><b>New York</b></p>
+      <p class="w3-opacity">Fri 27 Nov 2016</p>
+      <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+      <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Buy Tickets</button>
+    </div>
+  </div>
+  <div class="w3-third w3-margin-bottom">
+    <img src="/w3images/paris.jpg" alt="Paris" style="width:100%" class="w3-hover-opacity">
+    <div class="w3-container w3-white">
+      <p><b>Paris</b></p>
+      <p class="w3-opacity">Sat 28 Nov 2016</p>
+      <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+      <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Buy Tickets</button>
+    </div>
+  </div>
+  <div class="w3-third w3-margin-bottom">
+    <img src="/w3images/sanfran.jpg" alt="San Francisco" style="width:100%" class="w3-hover-opacity">
+    <div class="w3-container w3-white">
+      <p><b>San Francisco</b></p>
+      <p class="w3-opacity">Sun 29 Nov 2016</p>
+      <p>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+      <button class="w3-button w3-black w3-margin-bottom" onclick="document.getElementById('ticketModal').style.display='block'">Buy Tickets</button>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+<!-- Ticket Modal -->
+<div id="ticketModal" class="w3-modal">
+<div class="w3-modal-content w3-animate-top w3-card-4">
+<header class="w3-container w3-teal w3-center w3-padding-32"> 
+  <span onclick="document.getElementById('ticketModal').style.display='none'" 
+ class="w3-button w3-teal w3-xlarge w3-display-topright">×</span>
+  <h2 class="w3-wide"><i class="fa fa-suitcase w3-margin-right"></i>Tickets</h2>
+</header>
+<div class="w3-container">
+  <p><label><i class="fa fa-shopping-cart"></i> Tickets, $15 per person</label></p>
+  <input class="w3-input w3-border" type="text" placeholder="How many?">
+  <p><label><i class="fa fa-user"></i> Send To</label></p>
+  <input class="w3-input w3-border" type="text" placeholder="Enter email">
+  <button class="w3-button w3-block w3-teal w3-padding-16 w3-section w3-right">PAY <i class="fa fa-check"></i></button>
+  <button class="w3-button w3-red w3-section" onclick="document.getElementById('ticketModal').style.display='none'">Close <i class="fa fa-remove"></i></button>
+  <p class="w3-right">Need <a href="#" class="w3-text-blue">help?</a></p>
+</div>
+</div>
+</div>
+
+<!-- The Contact Section -->
+<div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="contact">
+<h2 class="w3-wide w3-center">CONTACT</h2>
+<p class="w3-opacity w3-center"><i>Fan? Drop a note!</i></p>
+<div class="w3-row w3-padding-32">
+<div class="w3-col m6 w3-large w3-margin-bottom">
+  <i class="fa fa-map-marker" style="width:30px"></i> Chicago, US<br>
+  <i class="fa fa-phone" style="width:30px"></i> Phone: +00 151515<br>
+  <i class="fa fa-envelope" style="width:30px"> </i> Email: mail@mail.com<br>
+</div>
+<div class="w3-col m6">
+  <form action="/action_page.php" target="_blank">
+    <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+      <div class="w3-half">
+        <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+      </div>
+      <div class="w3-half">
+        <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
+      </div>
+    </div>
+    <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message">
+    <button class="w3-button w3-black w3-section w3-right" type="submit">SEND</button>
+  </form>
+</div>
+</div>
+</div>
+
+<!-- End Page Content -->
+</div>
+
+<!-- Image of location/map -->
+<img src="/w3images/map.jpg" class="w3-image w3-greyscale-min" style="width:100%">
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+<i class="fa fa-facebook-official w3-hover-opacity"></i>
+<i class="fa fa-instagram w3-hover-opacity"></i>
+<i class="fa fa-snapchat w3-hover-opacity"></i>
+<i class="fa fa-pinterest-p w3-hover-opacity"></i>
+<i class="fa fa-twitter w3-hover-opacity"></i>
+<i class="fa fa-linkedin w3-hover-opacity"></i>
+<p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+<script>
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+var i;
+var x = document.getElementsByClassName("mySlides");
+for (i = 0; i < x.length; i++) {
+x[i].style.display = "none";  
+}
+myIndex++;
+if (myIndex > x.length) {myIndex = 1}    
+x[myIndex-1].style.display = "block";  
+setTimeout(carousel, 4000);    
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+var x = document.getElementById("navDemo");
+if (x.className.indexOf("w3-show") == -1) {
+x.className += " w3-show";
+} else { 
+x.className = x.className.replace(" w3-show", "");
+}
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
+</script>
+
+</body>
+</html>
